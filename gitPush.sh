c2587024b2991b8e62397ef83a3a3ea8dd4a5e3d
CHAIN-MIRROR <a.mccaffrey@unb.ca>
2023-11-28 15:56:35 +0000

    update 2023-11-28 15:56:35

--- gitPush.sh
+++ gitPush.sh
@@ -5,6 +5,13 @@ source ~/.A-CHAIM.conf
 
 cd $ROOTDIR/ACHAIM_Stats
 
+devName="centos01-dev"
+host=$(hostname)
+
+if [ "$host" == "$devName" ]; then
+    git checkout dev
+fi
+
 git pull
 git add -A
 git commit -m "update $(date '+%Y-%m-%d %H:%M:%S')"
